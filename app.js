73fe48dda7206f927e200d95b61cf0c6fed5b796
William Daigle <daiglewilliam13@gmail.com>
2021-08-11 23:11:43 +0000

    add colors, fonts, and ssl certs that probably don't actually do anything

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app 					= express(); //app instead of express when calling functions
 const port 					= 3000; //manually define port
 const path 					= require('path'); //allows me to define a file-path for stylesheets a publicly accessible files
 
+
 app.use(express.static(__dirname + '/public')); //define directory for views folder which contains stylesheets, etc
 app.set('view engine', 'ejs'); //so you don't have to tpe .ejs for ever res.render command
 
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { 
 	res.render('main');
 })
 
@@ -15,6 +16,7 @@ app.get('/more', (req, res) =>{
 	res.render('more'); 
 })
 
-app.listen( port, () => { //start app 
+
+app.listen( 3000 || port, () => { //start app 
 	console.log(`App Started and listening on ${port}`); //Backticks necessary instead of '' so the port variable is correctly displayed
 });
