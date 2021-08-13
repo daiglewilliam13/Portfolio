794ba351d4ada5baf87ad3784ab8040264925b53
William Daigle <daiglewilliam13@gmail.com>
2021-08-13 22:21:39 +0000

    update starry background animation

--- public/js/main.js
+++ public/js/main.js
@@ -24,16 +24,17 @@ ctx.shadowBlur = 3;
 const numberOfStars = 350;
 const stars = [];
 const starSpeed = 0.05 * canvas.width;
-const xv = starSpeed * randomSign() * Math.random();
-const yv = Math.sqrt(Math.pow(starSpeed, 2) - Math.pow(xv, 2)) * randomSign();
+const xv = starSpeed * randomSign() * Math.random(); // make stars move same direction
+const yv = Math.sqrt(Math.pow(starSpeed, 2) - Math.pow(xv, 2)) * randomSign(); //pythagorean theorem to calculate y speed
 
-for (let i = 0; i < numberOfStars; i++) {
+for (let i = 0; i < numberOfStars; i++) { //generate stars to display
+	let speedMult = Math.random() * 1.5 + .05; //changes the multiplier for star speed
 	stars[i] = {
 		r: getRandomInt(3),
 		x: Math.floor(Math.random() * width),
 		y: Math.floor(Math.random() * height),
-		xv: xv,
-		yv: yv,
+		xv: xv * speedMult,
+		yv: yv * speedMult
 	};
 }
 
@@ -42,15 +43,24 @@ let timeDelta,
 const loop = (timeNow) => {
 	timeDelta = timeNow - timeLast;
 	timeLast = timeNow;
-	ctx.clearRect(0,0,width, height);
+	ctx.clearRect(0, 0, width, height);
 	for (let i = 0; i < stars.length; i++) {
 		ctx.beginPath();
 		ctx.fillRect(stars[i].x, stars[i].y, stars[i].r, stars[i].r);
-		stars[i].x += stars[i].xv * timeDelta * 0.001;
-		stars[i].y += stars[i].yv * timeDelta * 0.001;
+		stars[i].x += stars[i].xv * timeDelta * 0.001; //update x position
+		stars[i].y += stars[i].yv * timeDelta * 0.001; //update y position
+		if (stars[i].x < 0 - stars[i].r) {
+			//reposition stars when they move off screen
+			stars[i].x = width + stars[i].r;
+		} else if (stars[i].x > width + stars[i].r) {
+			stars[i].x = 0 - stars[i].r;
+		}
+		if (stars[i].y < 0 - stars[i].r) {
+			stars[i].y = height + stars[i].r;
+		} else if (stars[i].y > height + stars[i].r) {
+			stars[i].y = 0 - stars[i].r;
+		}
 	}
-	
 	requestAnimationFrame(loop);
-	
 };
 requestAnimationFrame(loop);
