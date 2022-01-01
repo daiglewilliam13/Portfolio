3f6f9597edea17cb144b6800d2700866444d0570
William Daigle <daiglewilliam13@gmail.com>
2022-01-01 02:33:58 +0000

    organize js

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,37 @@
-console.log('connected');
+// LANDING AND NAVBAR
+const landingBars = Array.from(document.getElementsByClassName('bar'));
+
+const assignColorAnimation = (el, duration) => {  //assigns random color to landing bar
+	const colorArr = ['color-1', 'color-2', 'color-3', 'color-4', 'color-5']; 
+	let chosenColor, glow, star;
+	chosenColor = Math.floor(Math.random() * colorArr.length); //random array index
+	let starColor = 'star-' + colorArr[chosenColor];
+	glow = document.createElement('div');
+	star = document.createElement('div');
+	glow.classList.add('glow', colorArr[chosenColor]);
+	glow.style.animation = `move-left ${duration}ms ease-in forwards infinite`;
+	star.classList.add('star', starColor);
+	el.appendChild(glow);
+	el.appendChild(star);
+};
+
+const removeAllChildren = (el) => {
+	while (el.firstChild) {
+		el.removeChild(el.firstChild);
+	}
+};
+
+landingBars.forEach((bar) => {   //changes landing bar color and animation duration after each loop
+	(function loop() {
+		removeAllChildren(bar);
+		let duration;
+		duration = Math.round(Math.random() * 5000 + 5000);
+		assignColorAnimation(bar, duration);
+		setTimeout(() => {
+			loop();
+		}, duration);
+	})();
+});
 
 const addActiveClass = (el) => {
 	el.classList.toggle('nav-button-active');
@@ -12,7 +45,7 @@ const scrollToSection = (el) => {
 	});
 };
 
-window.onscroll = () => {
+window.onscroll = () => {       //changes nav bar highlighted section 
 	const sections = document.querySelectorAll('section');
 	const navLi = document.querySelectorAll('button.nav-button');
 	let current;
@@ -28,8 +61,22 @@ window.onscroll = () => {
 		if (li.innerText != current) li.classList.remove('nav-button-active');
 	});
 };
+const navItems = Array.from(document.getElementsByClassName('nav-button'));
+
+navItems.forEach((li) => {
+	li.addEventListener('click', () => {
+		scrollToSection(li.innerText);
+	});
+});
+
+// SKILLS SECTION
+
+const scrollNextArrow = document.getElementById('right-side-arrow');
+const scrollPrevArrow = document.getElementById('left-side-arrow');
 const skillCards = Array.from(document.getElementsByClassName('skill-card-header'));
-const showCard = (el) => {
+const skillItems = Array.from(document.getElementsByClassName('skill-li'));
+
+const showCard = (el) => { //when button is clicked, hide all cards that don't match button text
 	skillCards.forEach((card) => {
 		card.textContent == el.textContent
 			? card.parentNode.classList.remove('hidden')
@@ -46,20 +93,6 @@ const showCard = (el) => {
 	});
 };
 
-const navItems = Array.from(document.getElementsByClassName('nav-button'));
-navItems.forEach((li) => {
-	li.addEventListener('click', () => {
-		scrollToSection(li.innerText);
-	});
-});
-
-const skillItems = Array.from(document.getElementsByClassName('skill-li'));
-skillItems.forEach((li) => {
-	li.addEventListener('click', () => {
-		showCard(li);
-	});
-});
-
 const currentEl = () => {
 	let current;
 	skillItems.forEach((li, index) => {
@@ -68,16 +101,20 @@ const currentEl = () => {
 	return current;
 };
 
-const scrollNextArrow = document.getElementById('right-side-arrow');
-const scrollPrevArrow = document.getElementById('left-side-arrow');
-const scrollNext = (el, arr) => {
+const scrollNext = (el, arr) => {  
 	let next = el.nextElementSibling?.nextElementSibling;
-	next ? showCard(next) : showCard(skillItems[0]);
+	next ? showCard(next) : showCard(skillItems[0]); 
 };
 const scrollPrev = (el) => {
 	let prev = el.previousElementSibling?.previousElementSibling;
 	prev ? showCard(prev) : showCard(skillItems[skillItems.length - 1]);
 };
+
+skillItems.forEach((li) => {
+	li.addEventListener('click', () => {
+		showCard(li);
+	});
+});
 scrollNextArrow.addEventListener('click', () => {
 	scrollNext(currentEl());
 });
@@ -85,13 +122,20 @@ scrollPrevArrow.addEventListener('click', () => {
 	scrollPrev(currentEl());
 });
 
+// SAMPLE WORK SECTION
+
 const displayCards = Array.from(document.getElementsByClassName('work-display-card'));
 const positionCards = Array.from(document.getElementsByClassName('position-card'));
+const workSelectors = Array.from(document.getElementsByClassName('work-selector'));
+const positionWrapper = document.getElementById('position-card-wrapper');
+const displayWrapper = document.getElementById('display-card-wrapper');
+const quoters = Array.from(document.getElementsByClassName('quoter'));
+const quotes = Array.from(document.getElementsByClassName('quote'));
 
-const showSelection = (el) => {
+const showSelection = (el) => {   //when button is clicked, hide all cards that don't match button text
 	if (el.innerText == 'Show All') {
 		showAllCards();
-		animatePosition(displayCards, 0);
+		animatePosition(displayCards, 0);  //animate flexbox positioning by tying visible absolutely-positioned elements to hidden flexbox items 
 	} else {
 		displayCards.forEach((card, index) => {
 			let selection = el.innerText;
@@ -107,14 +151,7 @@ const showSelection = (el) => {
 	}
 };
 
-const workSelectors = Array.from(document.getElementsByClassName('work-selector'));
-workSelectors.forEach((sel) => {
-	sel.addEventListener('click', function () {
-		showSelection(sel);
-	});
-});
-
-const getCoords = (el) => {
+const getCoords = (el) => { //get number of px from top of document 
 	let box = el.getBoundingClientRect();
 	return {
 		top: box.top + window.pageYOffset,
@@ -134,9 +171,6 @@ const showAllCards = () => {
 	animatePosition(displayCards, 0);
 };
 
-let positionWrapper = document.getElementById('position-card-wrapper');
-let displayWrapper = document.getElementById('display-card-wrapper');
-
 const resizeEl = (elToSize, refEl) => {
 	elToSize.style.height = refEl.offsetHeight + 'px';
 	elToSize.style.width = refEl.offsetWidth + 'px';
@@ -154,7 +188,7 @@ const alignElems = (elemToMove, refElem, offset) => {
 };
 
 const animatePosition = (arr, delay) => {
-	setTimeout(() => {
+	setTimeout(() => {  
 		arr.forEach((item, index) => {
 			alignElems(item, positionCards[index], true);
 		});
@@ -162,22 +196,25 @@ const animatePosition = (arr, delay) => {
 	alignElems(displayWrapper, positionWrapper);
 };
 
+workSelectors.forEach((sel) => {
+	sel.addEventListener('click', function () {
+		showSelection(sel);
+	});
+});
+
 window.addEventListener('resize', function () {
-	animatePosition(displayCards, 500);
+	animatePosition(displayCards, 500); //add delay to animation so it doesn't fire excessively 
 });
 document.getElementById('position-card-wrapper').addEventListener('scroll', function () {
 	animatePosition(displayCards, 500);
 });
-
-const quoters = Array.from(document.getElementsByClassName('quoter'));
-const quotes = Array.from(document.getElementsByClassName('quote'));
 
 const slideQuotes = (arr, delay, faster) => {
 	const arrLength = arr.length;
 	let activeArrIndex = 0;
 
 	setInterval(() => {
-		let activeEl = arr[activeArrIndex];
+		let activeEl = arr[activeArrIndex];  //for tracking index position
 		arr.indexOf(activeEl) == arrLength - 1 ? (activeArrIndex = 0) : activeArrIndex++;
 		faster ? activeEl.classList.remove('slideInFaster') : activeEl.classList.remove('slideIn');
 		activeEl.classList.add('slideAway');
@@ -187,6 +224,9 @@ const slideQuotes = (arr, delay, faster) => {
 		arr[activeArrIndex].classList.remove('slideAway');
 	}, delay);
 };
+
+//CONTACT SECTION
+
 const postMessage = async (url = '', data = {}) => {
 	const req = await fetch(url, {
 		method: 'POST',
@@ -228,10 +268,6 @@ const sendMessage = () => {
 	}
 };
 
-document.getElementById('send-message-button').addEventListener('click', function () {
-	sendMessage();
-});
-
 function createRipple(event) {
 	const button = event.currentTarget;
 	const circle = document.createElement('span');
@@ -258,40 +294,7 @@ for (const button of buttons) {
 	button.addEventListener('click', createRipple);
 }
 
-const landingBars = Array.from(document.getElementsByClassName('bar'));
 
-
-const assignColorAnimation = (arr, duration) => {
-	const colorArr = ['color-1', 'color-2', 'color-3', 'color-4', 'color-5'];
-		let chosenColor, glow, star;
-		chosenColor = Math.floor(Math.random() * colorArr.length);
-		let starColor = 'star-' + colorArr[chosenColor];
-		glow = document.createElement('div');
-		star = document.createElement('div');
-		glow.classList.add("glow",colorArr[chosenColor]);
-		glow.style.animation = `move-left ${duration}ms ease-in forwards infinite`;
-		star.classList.add("star",starColor);
-		arr.appendChild(glow);
-		arr.appendChild(star);
-};
-
-const removeAllChildren = (el) => {
-	while (el.firstChild) {
-		el.removeChild(el.firstChild);
-	}
-}
-landingBars.forEach((bar)=>{
-	
-(function loop() {
-	removeAllChildren(bar);
-	let duration;
-	duration = Math.round(Math.random() * 5000 + 5000);
-	assignColorAnimation(bar, duration);
-	setTimeout(() => {
-		loop();
-	}, duration);
-})();
-})
 
 window.onload = function () {
 	animatePosition(displayCards, 0);
