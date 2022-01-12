a3052fce31c093ef5d4c8e19f645ffa0ef93ff3b
William Daigle <daiglewilliam13@gmail.com>
2022-01-12 19:43:24 +0000

    debug cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const Achievement = require("./models/achievement.js");
 
 app.use(cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://achievement-list.herokuapp.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
