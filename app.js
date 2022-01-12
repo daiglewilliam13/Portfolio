8d82d6388b17e210ee316913d1ae1ad615758c73
William Daigle <daiglewilliam13@gmail.com>
2022-01-12 18:52:26 +0000

    debug h10 crash

--- app.js
+++ app.js
@@ -92,6 +92,6 @@ app.post('/sendmessage', (req, res) => {
 	});
 });
 
-app.listen(port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log(`App Started and listening on ${port}`); 
 });
