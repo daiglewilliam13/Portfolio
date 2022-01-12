aa3a510c92c27c295b08a29bf06654b78898ce2f
William Daigle <daiglewilliam13@gmail.com>
2022-01-12 19:53:18 +0000

    debug cors

--- app.js
+++ app.js
@@ -9,16 +9,7 @@ const mongoose = require('mongoose');
 const cors = require('cors')
 const Achievement = require("./models/achievement.js");
 
-app.use(cors({
-	origin: ['https://portfolio-mqlie.run-us-west2.goorm.io/'],
-	credentials: true
-}));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-});
+app.use(cors());
 app.use(express.static(__dirname + '/public')); 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
